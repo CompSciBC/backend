eb78c5c73027cdcb15159bec14deab912280bf03
Matthew Ryan Granger <mattg1827@gmail.com>
2023-02-12 17:35:29 -0800

    draft

--- GuidebookModule/src/main/java/bmg/controller/GuidebookController.java
+++ GuidebookModule/src/main/java/bmg/controller/GuidebookController.java
@@ -4,5 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GuidebookController {
+    // write endpoint that does the following:
+            // call respective method in guidebook service that does:
+                        //connect to S3
+                        // retrieve json by propertyID (name of property object)
+                        // return that json in the response endpoint
+    // in frontend
+    // parse text into json obj
+    // pass to Guidebook Loader to display to the page
 
 }
