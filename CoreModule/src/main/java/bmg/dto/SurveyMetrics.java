4af28d882243c8190d1f378b23f82a5decea7a65
Hieu Do <hieu.do@bellevuecollege.edu>
2023-05-14 22:48:28 -0700

    host dash

---
+++ CoreModule/src/main/java/bmg/dto/SurveyMetrics.java
@@ -0,0 +1,10 @@
+package bmg.dto;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class SurveyMetrics {
+    private String hostId;
+    // private ? cumulativeMetrics;
+    private List<SurveyData> surveyResponses;
+}
